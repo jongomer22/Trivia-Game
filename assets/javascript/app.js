53c9ad2aafa0fd2ee095342ede073d94554a4b0b
JonGomer <jongomer22@gmail.com>
2019-07-28 00:22:55 -0700

    Html Complete Javascript update needed

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,12 @@
-console.log("Hello World!")
+$(document).ready(function(){
+
+    $("#q1").on("click", function(){
+        if (Input.value === false)
+        alert("Wrong");
+    })
+
+    })
+
+})
+
+// Need to finish my -Query code and get the quiz working properly
